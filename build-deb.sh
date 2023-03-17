8828fef41054a0e6dd03351695c6be90e79897bd
Jan Tomasek <jan@tomasek.cz>
2023-03-17 10:58:28 +0100

    move deb to current directory

--- build-deb.sh
+++ build-deb.sh
@@ -9,3 +9,9 @@ echo "done."
 cp -f LICENSE debian/copyright
 
 debuild -us -uc
+
+name=`cat debian/files |grep \.deb | sed 's/ .*$//'`
+
+echo "moving package file $name to current directory"
+mv "../$name" .
+
