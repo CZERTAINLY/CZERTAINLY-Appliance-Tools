90674722f51a3c5709879f5227f4da67968e3a5d
Jan Tomasek <jan@tomasek.cz>
2023-03-20 12:03:27 +0100

    c.run.3

--- build-deb.sh
+++ build-deb.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-set -e
-echo "-------------"
-set
-echo "-------------"
-git submodule update --init --recursive
+# test if repository was correctly initialized, if no initialize
+if [ ! -e etc/czertainly-ansible/roles/czertainly/tasks/main.yml ]
+then
+    git submodule update --init --recursive
+fi
 
 install='debian/install'
 echo -n "Creating $install: "
@@ -23,4 +23,6 @@ name=`cat debian/files |grep \.deb | sed 's/ .*$//'`
 # https://pmhahn.github.io/debian-oot-build/
 echo "moving package file $name to current directory"
 mv -f "../$name" .
+
+ls -l *deb
 
